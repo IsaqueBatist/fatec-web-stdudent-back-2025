a9c082905be9e8bb9151f0df8b8115a08001d12c
Isaque Barbosa <isaquebatista400@gmail.com>
2025-05-27 10:46:35 -0300

    :sparkles: feat: Adicionando PUT e DELETE

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -4,6 +4,7 @@ package br.gov.sp.fatec.itu.student.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,9 @@ import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.services.StudentService;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("students")
@@ -33,6 +37,19 @@ public class StudentController {
     public ResponseEntity<Student> save(@RequestBody Student student){
         return ResponseEntity.created(null)
                              .body(service.save(student));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> edit(@PathVariable Long id, @RequestBody Student student){
+        service.update(student, id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
+++ src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.repositories.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class StudentService {
@@ -14,12 +15,38 @@ public class StudentService {
     @Autowired
     private StudentRepository repository;
 
+    
+    //Pegar estudante
     public List<Student> getAll(){
         return repository.findAll();
     }
     
+    //Criar estudante
     public Student save(Student student){
         return repository.save(student);
     }
 
+    //Editar estudante
+    public void update(Student student, Long id){
+        Student aux  = repository.getReferenceById(id);
+        Student sux2 = repository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+
+        System.out.println(aux);
+        System.out.println(sux2);
+        aux.setCourse(student.getCourse());
+        aux.setName(student.getName());   
+
+        repository.save(aux);
+    }
+
+
+    //Deletar Estudante
+    public void delete(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }else{
+            throw new EntityNotFoundException("Aluno não cadastrado");
+        } 
+    }
+    
 }
