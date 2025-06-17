58b8edbc01831253e12e3b5ade6920571560a546
Isaque Barbosa <isaquebatista400@gmail.com>
2025-06-17 10:54:54 -0300

    :bug> fix: Cross origin error

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -4,6 +4,7 @@ package br.gov.sp.fatec.itu.student.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("students")
 public class StudentController {
 
